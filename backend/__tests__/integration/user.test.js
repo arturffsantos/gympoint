8df7aeaf5d78154d47e1dce14eed89cf4ddab85f
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2019-12-25 21:50:44 -0300

    fix(@api:user): remove endpoint to store user
    The register of admin user is not made by the API

--- backend/__tests__/integration/user.test.js
+++ backend/__tests__/integration/user.test.js
@@ -1,84 +1,8 @@
 import request from 'supertest';
-import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
 import factory from '../factories';
 import truncate from '../util/truncate';
-
-describe('User - create', () => {
-  beforeEach(async () => {
-    await truncate();
-  });
-
-  it('should encrypt user password when creating new user', async () => {
-    const user = await factory.create('User', {
-      password: '123456',
-    });
-
-    const compareHash = await bcrypt.compare('123456', user.password_hash);
-
-    expect(compareHash).toBe(true);
-  });
-
-  it('should be able to register', async () => {
-    const user = await factory.attrs('User');
-
-    const response = await request(app)
-      .post('/users')
-      .send(user);
-
-    expect(response.body).toHaveProperty('id');
-  });
-
-  it('should not be able to register with duplicated email', async () => {
-    const user = await factory.attrs('User');
-
-    await request(app)
-      .post('/users')
-      .send(user);
-
-    const response = await request(app)
-      .post('/users')
-      .send(user);
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should not be able to register with missing name', async () => {
-    const user = await factory.attrs('User');
-    delete user.name;
-
-    const response = await request(app)
-      .post('/users')
-      .send(user);
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should not be able to register with invalid email', async () => {
-    const user = await factory.attrs('User', {
-      email: 'invalid_email',
-    });
-
-    const response = await request(app)
-      .post('/users')
-      .send(user);
-
-    expect(response.status).toBe(400);
-  });
-
-  it('should not be able to register with invalid password', async () => {
-    const user = factory.attrs('User', {
-      password: '123',
-    });
-
-    const response = await request(app)
-      .post('/users')
-      .send(user);
-
-    expect(response.status).toBe(400);
-  });
-});
 
 describe('User - update', () => {
   let token;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,6 @@ import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
